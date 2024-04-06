5c1e4f6324b80daa1504a8676da72011bee7a793
Ildar Nasyrov <iledarnp@gmail.com>
2024-04-06 14:05:30 +0800

    add gnome appindicator

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -440,6 +440,8 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     gnome.gnome-tweaks
+    gnome.adwaita-icon-theme
+    gnomeExtensions.appindicator
     wget
   ];
 
