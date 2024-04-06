1afd69b17b660f60bd1baa5dd95fdb447e453ef6
Ildar Nasyrov <iledarnp@gmail.com>
2024-04-06 16:04:00 +0800

    reformatted with nixpkgs-fmt

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -6,7 +6,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
       <home-manager/nixos>
       <sops-nix/modules/sops>
@@ -93,7 +94,7 @@
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [
       firefox
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -101,7 +102,7 @@
     nixpkgs = {
       config = {
         allowUnfree = true;
-	allowUnfreePredicate = _: true;
+        allowUnfreePredicate = _: true;
       };
     };
 
@@ -120,7 +121,6 @@
       htop
       emacs
       git
-#      neovim
       keepassxc
       maestral
       surfraw
@@ -141,14 +141,12 @@
       teams-for-linux
       dconf2nix
       dropbox
-
       ripgrep
       fd
-
       gimp
-
       gparted
       ntfs3g
+      nixpkgs-fmt
     ];
     programs.bash.enable = true;
 
@@ -158,26 +156,26 @@
       historyLimit = 100000;
       plugins = with pkgs;
         [
-	  tmuxPlugins.yank
-	  tmuxPlugins.vim-tmux-navigator
-	  tmuxPlugins.sensible
-	  tmuxPlugins.sessionist
-	  tmuxPlugins.resurrect
-	  tmuxPlugins.pain-control
-	  tmuxPlugins.gruvbox
-	  tmuxPlugins.tmux-fzf
-	  tmuxPlugins.fzf-tmux-url
-	];
-	extraConfig = ''
-          setw -g mode-keys vi
-          unbind C-b
-          set -g prefix C-a
-          bind C-a send-prefix
-          bind-key C-a last-window
-	  set-option -sa terminal-features ',foot:RGB'
-	'';
+          tmuxPlugins.yank
+          tmuxPlugins.vim-tmux-navigator
+          tmuxPlugins.sensible
+          tmuxPlugins.sessionist
+          tmuxPlugins.resurrect
+          tmuxPlugins.pain-control
+          tmuxPlugins.gruvbox
+          tmuxPlugins.tmux-fzf
+          tmuxPlugins.fzf-tmux-url
+        ];
+      extraConfig = ''
+                  setw -g mode-keys vi
+                  unbind C-b
+                  set -g prefix C-a
+                  bind C-a send-prefix
+                  bind-key C-a last-window
+        	  set-option -sa terminal-features ',foot:RGB'
+        	'';
     };
-    
+
     services.emacs.enable = true;
 
     programs.gpg.enable = true;
@@ -199,167 +197,167 @@
       defaultEditor = true;
       plugins = with pkgs.vimPlugins; [
         fugitive
-	vim-rhubarb
-	vimagit
+        vim-rhubarb
+        vimagit
         {
-	  plugin = oil-nvim;
-	  type = "lua";
-	  config = ''
-	    require('oil').setup()
-	  '';
-	}
-	nvim-treesitter.withAllGrammars
+          plugin = oil-nvim;
+          type = "lua";
+          config = ''
+            	    require('oil').setup()
+            	  '';
+        }
+        nvim-treesitter.withAllGrammars
         nvim-treesitter-textobjects
 
-	gruvbox-material
-	vim-code-dark
-	papercolor-theme
-	mini-nvim
-	nvim-web-devicons
-	{
-	  plugin = nvim-tree-lua;
-	  type = "lua";
-	  config = ''
-	    -- disable netrw at the very start of your init.lua
-            vim.g.loaded_netrw = 1
-            vim.g.loaded_netrwPlugin = 1
+        gruvbox-material
+        vim-code-dark
+        papercolor-theme
+        mini-nvim
+        nvim-web-devicons
+        {
+          plugin = nvim-tree-lua;
+          type = "lua";
+          config = ''
+            	    -- disable netrw at the very start of your init.lua
+                        vim.g.loaded_netrw = 1
+                        vim.g.loaded_netrwPlugin = 1
 
-            -- optionally enable 24-bit colour
-            -- vim.opt.termguicolors = true
+                        -- optionally enable 24-bit colour
+                        -- vim.opt.termguicolors = true
 
-            -- empty setup using defaults
-	    require('nvim-tree').setup()
-            vim.api.nvim_set_keymap('n', '<leader>\\', [[<cmd>NvimTreeToggle<CR>]], { })
-	    vim.api.nvim_set_keymap('n', '<leader>\\\\', [[<cmd>NvimTreeFindFile<CR>]], { })
-	  '';
-	}
-	vim-tmux-navigator
-	vim-tmux-clipboard
-	vim-unimpaired
-	vim-dadbod
-	vim-dadbod-ui
-	vim-dadbod-completion
+                        -- empty setup using defaults
+            	    require('nvim-tree').setup()
+                        vim.api.nvim_set_keymap('n', '<leader>\\', [[<cmd>NvimTreeToggle<CR>]], { })
+            	    vim.api.nvim_set_keymap('n', '<leader>\\\\', [[<cmd>NvimTreeFindFile<CR>]], { })
+            	  '';
+        }
+        vim-tmux-navigator
+        vim-tmux-clipboard
+        vim-unimpaired
+        vim-dadbod
+        vim-dadbod-ui
+        vim-dadbod-completion
 
-	plenary-nvim
-	telescope-nvim
-	{
-	  plugin = fzf-lua;
-	  type = "lua";
-	  config = ''
-	    vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>tt", "<cmd>lua require('fzf-lua').tabs()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>gg", "<cmd>lua require('fzf-lua').grep()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>gl", "<cmd>lua require('fzf-lua').grep_last()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>gC", "<cmd>lua require('fzf-lua').grep_cword()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>gc", "<cmd>lua require('fzf-lua').grep_cWORD()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>gb", "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>jj", "<cmd>lua require('fzf-lua').jumps()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>rr", "<cmd>lua require('fzf-lua').registers()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>cc", "<cmd>lua require('fzf-lua').changes()<CR>", { silent = true })
-	    vim.keymap.set("n", "<leader>bb", "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
+        plenary-nvim
+        telescope-nvim
+        {
+          plugin = fzf-lua;
+          type = "lua";
+          config = ''
+            	    vim.keymap.set("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>tt", "<cmd>lua require('fzf-lua').tabs()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>gg", "<cmd>lua require('fzf-lua').grep()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>gl", "<cmd>lua require('fzf-lua').grep_last()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>gC", "<cmd>lua require('fzf-lua').grep_cword()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>gc", "<cmd>lua require('fzf-lua').grep_cWORD()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>gb", "<cmd>lua require('fzf-lua').lgrep_curbuf()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>jj", "<cmd>lua require('fzf-lua').jumps()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>rr", "<cmd>lua require('fzf-lua').registers()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>cc", "<cmd>lua require('fzf-lua').changes()<CR>", { silent = true })
+            	    vim.keymap.set("n", "<leader>bb", "<cmd>lua require('fzf-lua').buffers()<CR>", { silent = true })
 
-	    local actions = require "fzf-lua.actions"
+            	    local actions = require "fzf-lua.actions"
 
-	    require'fzf-lua'.setup{
-	      actions = {
-                -- These override the default tables completely
-                -- no need to set to `false` to disable an action
-                -- delete or modify is sufficient
+            	    require'fzf-lua'.setup{
+            	      actions = {
+                            -- These override the default tables completely
+                            -- no need to set to `false` to disable an action
+                            -- delete or modify is sufficient
 
-                files = {
+                            files = {
 
-                  -- providers that inherit these actions:
-                  --   files, git_files, git_status, grep, lsp
-                  --   oldfiles, quickfix, loclist, tags, btags
-                  --   args
-                  -- default action opens a single selection
-                  -- or sends multiple selection to quickfix
-                  -- replace the default action with the below
-                  -- to open all files whether single or multiple
-                  -- ["default"]     = actions.file_edit,
+                              -- providers that inherit these actions:
+                              --   files, git_files, git_status, grep, lsp
+                              --   oldfiles, quickfix, loclist, tags, btags
+                              --   args
+                              -- default action opens a single selection
+                              -- or sends multiple selection to quickfix
+                              -- replace the default action with the below
+                              -- to open all files whether single or multiple
+                              -- ["default"]     = actions.file_edit,
 
-                  ["default"]     = actions.file_edit_or_qf,
-                  ["ctrl-s"]      = actions.file_split,
-                  ["ctrl-v"]      = actions.file_vsplit,
-                  ["ctrl-t"]      = actions.file_tabedit,
-                  ["alt-q"]       = actions.file_sel_to_qf,
-                  ["alt-l"]       = actions.file_sel_to_ll,
-                  ["ctrl-g"]      = actions.toggle_ignore,
+                              ["default"]     = actions.file_edit_or_qf,
+                              ["ctrl-s"]      = actions.file_split,
+                              ["ctrl-v"]      = actions.file_vsplit,
+                              ["ctrl-t"]      = actions.file_tabedit,
+                              ["alt-q"]       = actions.file_sel_to_qf,
+                              ["alt-l"]       = actions.file_sel_to_ll,
+                              ["ctrl-g"]      = actions.toggle_ignore,
 
-                },
-              },
-	    }
+                            },
+                          },
+            	    }
 
-	  '';
-	}
-	
-	{
-	  plugin = nvim-surround;
-	  type = "lua";
-	  config = ''
-	    require('nvim-surround').setup()
-	  '';
-	}
+            	  '';
+        }
 
-	{
-	  plugin = lualine-nvim;
-	  type = "lua";
-	  config = ''
-	    require('lualine').setup({
-	      extensions = { "fugitive", "fzf", "quickfix" },
-	      options = {
-	        icons_enabled = true,
-		theme = 'auto',
-	      },
-	      tabline = {},
-	    })
-	  '';
-	}
+        {
+          plugin = nvim-surround;
+          type = "lua";
+          config = ''
+            	    require('nvim-surround').setup()
+            	  '';
+        }
 
-	{
-	  plugin = bufferline-nvim;
-	  type = "lua";
-	  config = ''
-	    require('bufferline').setup({
-	      options = {
-	        mode = "tabs",
-		numbers = "ordinal",
-	      },
-	    })
-	    vim.keymap.set("n", "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>")
-	    vim.keymap.set("n", "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>")
-	    vim.keymap.set("n", "<leader>3", "<cmd>BufferLineGoToBuffer 3<CR>")
-	    vim.keymap.set("n", "<leader>4", "<cmd>BufferLineGoToBuffer 4<CR>")
-	    vim.keymap.set("n", "<leader>5", "<cmd>BufferLineGoToBuffer 5<CR>")
-	    vim.keymap.set("n", "<leader>6", "<cmd>BufferLineGoToBuffer 6<CR>")
-	    vim.keymap.set("n", "<leader>7", "<cmd>BufferLineGoToBuffer 7<CR>")
-	    vim.keymap.set("n", "<leader>8", "<cmd>BufferLineGoToBuffer 8<CR>")
-	    vim.keymap.set("n", "<leader>9", "<cmd>BufferLineGoToBuffer 9<CR>")
-	  '';
-	}
+        {
+          plugin = lualine-nvim;
+          type = "lua";
+          config = ''
+            	    require('lualine').setup({
+            	      extensions = { "fugitive", "fzf", "quickfix" },
+            	      options = {
+            	        icons_enabled = true,
+            		theme = 'auto',
+            	      },
+            	      tabline = {},
+            	    })
+            	  '';
+        }
 
-	{
-	  plugin = comment-nvim;
-	  type = "lua";
-	  config = ''
-	    require('Comment').setup()
-	  '';
-	}
+        {
+          plugin = bufferline-nvim;
+          type = "lua";
+          config = ''
+            	    require('bufferline').setup({
+            	      options = {
+            	        mode = "tabs",
+            		numbers = "ordinal",
+            	      },
+            	    })
+            	    vim.keymap.set("n", "<leader>1", "<cmd>BufferLineGoToBuffer 1<CR>")
+            	    vim.keymap.set("n", "<leader>2", "<cmd>BufferLineGoToBuffer 2<CR>")
+            	    vim.keymap.set("n", "<leader>3", "<cmd>BufferLineGoToBuffer 3<CR>")
+            	    vim.keymap.set("n", "<leader>4", "<cmd>BufferLineGoToBuffer 4<CR>")
+            	    vim.keymap.set("n", "<leader>5", "<cmd>BufferLineGoToBuffer 5<CR>")
+            	    vim.keymap.set("n", "<leader>6", "<cmd>BufferLineGoToBuffer 6<CR>")
+            	    vim.keymap.set("n", "<leader>7", "<cmd>BufferLineGoToBuffer 7<CR>")
+            	    vim.keymap.set("n", "<leader>8", "<cmd>BufferLineGoToBuffer 8<CR>")
+            	    vim.keymap.set("n", "<leader>9", "<cmd>BufferLineGoToBuffer 9<CR>")
+            	  '';
+        }
 
-	{
-	  plugin = indent-blankline-nvim;
-	  type = "lua";
-	  config = ''
-	    require('ibl').setup()
-	  '';
-	}
+        {
+          plugin = comment-nvim;
+          type = "lua";
+          config = ''
+            	    require('Comment').setup()
+            	  '';
+        }
+
+        {
+          plugin = indent-blankline-nvim;
+          type = "lua";
+          config = ''
+            	    require('ibl').setup()
+            	  '';
+        }
 
         vim-signify
 
-	{
-	  plugin = aerial-nvim;
-	  type = "lua";
-	  config = ''
+        {
+          plugin = aerial-nvim;
+          type = "lua";
+          config = ''
 
             require("aerial").setup({
                 on_attach = function(bufnr)
@@ -372,37 +370,37 @@
             })
 
 	  '';
-	}
+        }
 
       ];
       extraConfig = ''
-        colorscheme PaperColor
+                colorscheme PaperColor
 
-        " Highlight on yank
-        augroup YankHighlight
-        autocmd!
-        autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-        augroup end
+                " Highlight on yank
+                augroup YankHighlight
+                autocmd!
+                autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+                augroup end
 
-        au FileType python setlocal equalprg=black\ -\ 2>/dev/null
+                au FileType python setlocal equalprg=black\ -\ 2>/dev/null
 
-        set clipboard+=unnamedplus
-        set clipboard^=unnamed,unnamedplus
+                "set clipboard+=unnamedplus
+                "set clipboard^=unnamed,unnamedplus
 
-        let g:db_ui_use_nerd_fonts = 1
+                let g:db_ui_use_nerd_fonts = 1
 
-	" --Zooming Vim Window Splits like a Pro
-	noremap Zz <c-w>_ \| <c-w>\|
-	noremap Zo <c-w>=
+        	" --Zooming Vim Window Splits like a Pro
+        	noremap Zz <c-w>_ \| <c-w>\|
+        	noremap Zo <c-w>=
       '';
       extraLuaConfig = ''
-	--Remap space as leader key
-        vim.g.mapleader = ' '
-        vim.g.maplocalleader = ','
+        	--Remap space as leader key
+                vim.g.mapleader = ' '
+                vim.g.maplocalleader = ','
 
-        vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
-        vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>")
-        vim.keymap.set("n", "<leader><Tab>", "<cmd>b#<cr>")
+                vim.keymap.set("n", "<leader>w", "<cmd>w<cr>")
+                vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>")
+                vim.keymap.set("n", "<leader><Tab>", "<cmd>b#<cr>")
       '';
     };
 
@@ -414,8 +412,8 @@
     programs.ssh.matchBlocks = {
       kepiProd = {
         hostname = "139.162.11.95";
-	user = "prod";
-	identityFile = "/home/ildar/.ssh/id_ed25519";
+        user = "prod";
+        identityFile = "/home/ildar/.ssh/id_ed25519";
       };
     };
 
@@ -427,9 +425,9 @@
       enable = true;
       settings = {
         main = {
-	  font = "Hack Nerd Font Mono";
-	  dpi-aware = "yes";
-	};
+          font = "Hack Nerd Font Mono";
+          dpi-aware = "yes";
+        };
       };
     };
   };
